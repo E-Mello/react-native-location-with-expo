0c849e69d5b69ca98b404d6ca84baee0127cc9a2
Édio Melo <e.mello.dev@gmail.com>
2024-04-08 22:57:25 -0300

    Learning to use expo-location to work with location
    Only the basic commands, however, serve as a basis

--- App.tsx
+++ App.tsx
@@ -1,10 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import { useEffect, useState } from "react";
+import * as Location from "expo-location";
+import { LocationObject } from "expo-location";
 
 export default function App() {
+  const [location, setLocation] = useState<LocationObject>();
+  const [address, setAddress] = useState();
+
+  useEffect(() => {
+    const getPermissions = async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        console.log("Please grant location permissions");
+        return;
+      }
+
+      let currentLocation = await Location.getCurrentPositionAsync({});
+      setLocation(currentLocation);
+      console.log("Location:");
+      console.log(currentLocation);
+    };
+    getPermissions();
+  }, []);
+
+  const geocode = async () => {
+    const geocodedLocation = await Location.geocodeAsync(address);
+    const teste = JSON.stringify(geocodedLocation);
+    console.log("Geocode Address:");
+    console.log(geocodedLocation);
+    console.log(teste);
+  };
+
+  const reverseGeocode = async () => {
+    const reverseGeocodeAddress = await Location.reverseGeocodeAsync({
+      longitude: location?.coords.latitude,
+      latitude: location?.coords.latitude,
+    });
+
+    console.log("reverse geocoded");
+    console.log(reverseGeocodeAddress);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <TextInput
+        placeholder="Address"
+        value={address}
+        onChangeText={setAddress}
+      />
+      <Button title="Geocode Address" onPress={geocode} />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +58,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
